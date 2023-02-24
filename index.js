c1bb61e64080ae030e414a1a899e7d8fd513dd00
SollyBunny <sollybunn@gmail.com>
2023-02-24 01:04:47 +0000

    i make so many typos its funny

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ client.once("ready", async () => {
 	});
 	log.info(`Ready as ${client.user.tag}`);
 	let commands = [];
-	Object.values(client.cmds).forEach(i => {
+	Object.keys(client.cmds).forEach(i => {
 		if (i.admin) return;
 		let command = {
 			name: i,
